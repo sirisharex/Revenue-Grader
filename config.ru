71e900bbae902ec66b63578dab6d8b8daef50324
Sirisha Godina <sirisha.godina@icicletech.com>
2012-03-09 14:02:47 +0530

    Initial commit

---
+++ config.ru
@@ -0,0 +1,4 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run RevenueGrader07032012::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevenueGrader07032012::Application.config.secret_token = '9a777f25913531577ee55c6067599f53f4f8ac27df8b767d2930e9d5042d92c91e4b3688204a6992df34339452902124beaebec25777df29539e77628fe18d61'
